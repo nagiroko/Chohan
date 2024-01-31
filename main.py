278068d0fe6a2c2bdd3f1d7e1b230ceeab5d7fb3
nagiroko <hoobrajrohit@yahoo.com>
2024-01-31 11:48:30 -0500

    roll
    made a function that generates 2 numbers from 1 to six. Then those numbers are added and it checks if the number type matches the one entered

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+
+def roll(type):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    result = dice2 + dice1
+    print(result)
+    if result % 2 == 0 and type == "even":
+        print("you win")
+    elif result % 2 == 1 and type == "odd":
+        print("you win")
+    else:
+        print("you lose")
+
+roll("odd")
