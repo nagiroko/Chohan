74ab7c4a5e0da52365639418976f290fdf902d03
nagiroko <hoobrajrohit@yahoo.com>
2024-02-02 21:06:00 -0500

    Even
    added 2 seperate functions for the odd and even system and gave them corresponding buttons

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import sys
 import random
 
 
-def roll(type):
+def Oddroll():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     result = dice2 + dice1
     print(result)
-    if result % 2 == 0 and type == "even":
+    if result % 2 == 1:
         print("you win")
-    elif result % 2 == 1 and type == "odd":
+    else:
+        print("you lose")
+
+def Evenroll():
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    result = dice2 + dice1
+    print(result)
+    if result % 2 == 0:
         print("you win")
     else:
         print("you lose")
--- ui.py
+++ ui.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-from main import roll
+from main import Oddroll
 from main import Quit
+from main import Evenroll
 game = Tk()
 
 game.geometry("700x350")
 
 
-ttk.Button(game,text="Odd",command=roll("odd")).pack(pady=20)
-ttk.Button(game,text="Quit",command=Quit).pack(pady=40)
+ttk.Button(game,text="Odd",command=Oddroll).pack(pady=20)
+ttk.Button(game,text="Even",command=Evenroll).pack(pady=40)
+ttk.Button(game,text="Quit",command=Quit).pack(pady=60)
 game.mainloop()
