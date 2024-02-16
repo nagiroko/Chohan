35965ccb3cd34000f284e3a957b3c721faa312de
nagiroko <hoobrajrohit@yahoo.com>
2024-02-16 11:39:51 -0500

    Disable
    made it so once you press roll the odd and even buttons become active and vice versa

--- ui.py
+++ ui.py
@@ -18,10 +18,10 @@ rolled = Label(game,text="")
 result = Label(game,text="")
 
 #Buttons
-roll = Button(game,text= "Roll",command= lambda: [Roll(),Results()])
+roll = Button(game,text= "Roll",command= lambda: [State(),Roll(),Results()])
 quit = Button(game,text= "Quit",command= Quit)
-evenroll = Button(game,text= "EvenRoll",command= lambda: [Evenroll(),Results()])
-oddroll = Button(game,text= "OddRoll",command= lambda: [Oddroll(),Results()])
+evenroll = Button(game,text= "EvenRoll",command= lambda: [State(),Evenroll(),Results()])
+oddroll = Button(game,text= "OddRoll",command= lambda: [State(),Oddroll(),Results()])
 
 desc.pack()
 rolled.pack(pady= 10)
@@ -32,11 +32,27 @@ quit.place(relx= 0.5,rely= 0.5,anchor= CENTER)
 evenroll.place(relx=0.4,rely= 0.9,anchor= CENTER)
 oddroll.place(relx=0.6,rely= 0.9,anchor= CENTER)
 
+evenroll.config(state= DISABLED)
+oddroll.config(state= DISABLED)
 def Results():
     from main import dice
     from main import Result
     rolled.config(text=dice)
     result.config(text=Result)
+
+state = 0
+def State():
+    global state
+    if state == 0:
+        evenroll.config(state=ACTIVE)
+        oddroll.config(state=ACTIVE)
+        roll.config(state=DISABLED)
+        state = 1
+    else:
+        evenroll.config(state=DISABLED)
+        oddroll.config(state=DISABLED)
+        roll.config(state=ACTIVE)
+        state = 0
 
 
 game.mainloop()
