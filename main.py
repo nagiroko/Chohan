3d501bf3296e24dfce9e64165ca055aee0cad513
nagiroko <hoobrajrohit@yahoo.com>
2024-02-19 13:25:56 -0500

    track
    added a label that tracks the amount of player/dealer wins

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ Result = str("")
 dice = str("")
 die1 = 0
 die2 = 0
+PlayerWins = 0
+DealerWins = 0
 def Oddroll():
     global  die1
     global  die2
+    global PlayerWins
+    global DealerWins
     result = die2 + die1
     global dice
     dice = "You rolled a " + str(die1) + " and a " + str(die2)
     global Result
     if result % 2 == 1:
+       PlayerWins += 1
        Result = "you win"
     else:
+       DealerWins += 1
        Result = "you lose"
 
 
@@ -23,13 +29,17 @@ def Oddroll():
 def Evenroll():
     global  die1
     global  die2
+    global PlayerWins
+    global DealerWins
     result = die2 + die1
     global dice
     dice = "You rolled a " + str(die1) + " and a " + str(die2)
     global Result
     if result % 2 == 0:
+        PlayerWins += 1
         Result = "you win"
     else:
+        DealerWins += 1
         Result = "you lose"
 
 
@@ -46,6 +56,3 @@ def Roll():
 def Quit():
     sys.exit()
 
-Roll()
-Evenroll()
-Oddroll()
--- ui.py
+++ ui.py
@@ -15,16 +15,18 @@ game.geometry("700x350")
 #labels
 desc = Label(game,text="Press roll to start")
 result = Label(game,text="")
+total = Label(game,text= "")
 diceRolled = Label(game,text='',font=("times",30))
 
 #Buttons
 roll = Button(game,text= "Roll",command= lambda: [State(),Roll(),Results()])
 quit = Button(game,text= "Quit",command= Quit)
-evenroll = Button(game,text= "EvenRoll",command= lambda: [State(),Evenroll(),Results(),diceFaces()])
-oddroll = Button(game,text= "OddRoll",command= lambda: [State(),Oddroll(),Results(),diceFaces()])
+evenroll = Button(game,text= "EvenRoll",command= lambda: [Evenroll(),State(),Results(),diceFaces()])
+oddroll = Button(game,text= "OddRoll",command= lambda: [Oddroll(),State(),Results(),diceFaces()])
 
 desc.pack()
 result.pack(pady= 10)
+total.pack()
 diceRolled.pack(pady= 35)
 
 roll.place(relx= 0.5,rely= 0.8,anchor= CENTER)
@@ -42,17 +44,21 @@ def Results():
 state = 0
 def State():
     global state
+    from main import PlayerWins
+    from main import DealerWins
     if state == 0:
         evenroll.config(state=ACTIVE)
         oddroll.config(state=ACTIVE)
         roll.config(state=DISABLED)
         desc.config(text="Press either odd or even to bet on the dice's value")
+        total.config(text="")
         state = 1
     else:
         evenroll.config(state=DISABLED)
         oddroll.config(state=DISABLED)
         roll.config(state=ACTIVE)
         desc.config(text="Press roll to play again")
+        total.config(text = 'Player {}:{} Dealer'.format(PlayerWins,DealerWins))
         state = 0
 def diceFaces():
     from main import die1
